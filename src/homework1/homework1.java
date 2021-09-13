3883c009fe1346e8d6a96dd5f2299e618054abdd
kellynguyvn <80297074+kellynguyvn@users.noreply.github.com>
2021-09-12 23:45:23 -0700

    Update homework1.java
    Third commit including javadoc

--- src/homework1/homework1.java
+++ src/homework1/homework1.java
@@ -1,10 +1,13 @@
 package homework1;
 
 public class homework1 {
+    //Class initializing instance variable
     String Homework;
     public void student(String home) {
         Homework=home;
     }
+    
+    //Prints object based off class homework1
     public static void main(String[] args){
         Homework one=new Homework("commit");
         System.out.println(one);
